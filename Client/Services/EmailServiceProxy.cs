1ba4abb2b467ec7b8b39f437eef04e47f3620732
ssingh <ssingh@ntnanderson.com>
2025-07-16 08:51:35 -0400

    Initial commit

---
+++ Client/Services/EmailServiceProxy.cs
@@ -0,0 +1,35 @@
+﻿using DynamicFormsApp.Shared.Services;
+using DynamicFormsApp.Shared.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace DynamicFormsApp.Client.Services
+{
+    public class EmailServiceProxy : IEmailService
+    {
+        private readonly HttpClient _httpClient;
+
+        public EmailServiceProxy(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task SendBugReportEmail(EmailModel email)
+        {
+            await _httpClient.PostAsJsonAsync("api/email/bug/", email);
+        }
+
+        public async Task SendFormResponseNotification(string toEmail, string formName, int formId)
+        {
+            var payload = new { toEmail, formName, formId };
+            await _httpClient.PostAsJsonAsync("api/email/formresponse", payload);
+        }
+
+        public async Task SendFormShareNotification(string toEmail, string firstName, string formName, string? description, int formId, string sharedBy, string ownerEmail)
+        {
+            var payload = new { toEmail, firstName, formName, description, formId, sharedBy, ownerEmail };
+            await _httpClient.PostAsJsonAsync("api/email/formshare", payload);
+        }
+    }
+}
